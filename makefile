2b8d2bfcc588ce8311e351eb843ea50ba8cf0142
OmarAYousry <omarahm3d19@gmail.com>
2016-03-18 00:48:38 +0200

    Just Gitting

---
+++ makefile
@@ -0,0 +1,4 @@
+default: Main
+
+Main:
+	g++ -static src/Main.cpp -o bin/Application
---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+#include "../headers/Main.h"
+
+int main(int argc, char const *argv[]) {
+
+std::cout << "I am a msg kpa" << std::endl;
+int x[] = { 16, 2, 77, 40, 12071 };
+  std::cout << x[0];
+  while (true) {
+    /* code */
+  }
+  return 0;
+}
